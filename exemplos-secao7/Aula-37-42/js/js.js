acfcb78dc8165ab474c5f74db4f6e9f5cfe1d17b
JessPergentino <jessicapergentino@hotmail.com>
2018-07-18 12:23:02 -0300

    Ajuste de pastas - Exemplos das aulas 37 a 42

---
+++ exemplos-secao7/Aula-37-42/js/js.js
@@ -0,0 +1,15 @@
+//Declaração de Variável - Comentario de uma linha
+var nome;
+
+nome = "Jessica"; // Tipo String
+var idade = 21; // Tipo Integer
+var pretencaoSalarial = 5856.54; //Tipo Double
+var valeReeicao = true; // Tipo Boolean
+
+/**
+ * Esse é um
+ * comentario
+ * em varias
+ * linhas
+ */
+
