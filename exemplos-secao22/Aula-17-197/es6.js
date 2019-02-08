39c631ad793159cbcb4f78234fdd8fdabde80cc1
JessPergentino <jessicapergentino@hotmail.com>
2019-02-08 11:31:17 -0300

    Praticas com map

--- exemplos-secao22/Aula-17-197/es6.js
+++ exemplos-secao22/Aula-17-197/es6.js
@@ -71,4 +71,103 @@ pessoa.nome = "Demetrius";
 pessoa.cpf = "00000000000";
 console.log(pessoa);
 
-//Map
+//Map - Parte 1
+var nome = "Jessica",
+    numero = 21,
+    ativo = true,
+    itens = ["prod1","prod2","prod3"],
+    funcao = function(){
+        console.log("Sou uma função");
+    },
+    objeto = {nome: "pessoa", cpf:"21352132"},
+    expRegular = /teste/gi;
+//Declarar um map
+const map = new Map();
+//Adicionar itens
+map.set("nome", nome);
+map.set(123, numero);
+map.set(ativo, false);
+map.set(itens, itens);
+map.set(funcao, funcao);
+map.set(objeto, 12465);
+let exp = map.set(expRegular, expRegular);
+console.log("Exibir o retorno e o tipo");
+console.log(exp, Object.prototype.toString.call(exp));
+
+exp.set("novo elemento", 123456);
+console.log(map == exp);
+console.log(map);
+console.log(map.size);
+
+//Pegar os valores do map
+let elemento1 = map.get("nome");
+let elemento2 = map.get(123);
+let elemento3 = map.get(ativo);
+let elemento4 = map.get(itens);
+let elemento5 = map.get(funcao);
+let elemento6 = map.get(objeto);
+let elemento7 = map.get(expRegular);
+
+console.log(elemento1, typeof elemento1);
+console.log(elemento2, typeof elemento2);
+console.log(elemento3, typeof elemento3);
+console.log(elemento4, typeof elemento4);
+console.log(elemento5, typeof elemento5);
+console.log(elemento6, typeof elemento6);
+console.log(elemento7, typeof elemento7);
+console.log(map.get("uma chave qualquer"));
+
+//Map - Parte 2
+//deletadando um elemento
+let retorno = map.delete("nome");
+console.log(retorno);
+console.log(map.delete(123));
+console.log(map.delete("chave q nao existe"));
+//Iteração sobre o map
+map.forEach(function(valor,chave,map){
+    console.log(chave,"-",valor);
+})
+//Verificando uma chave
+console.log(map.has(funcao));
+console.log(map.has("nome"));
+console.log(map.has("chave q nao existe"));
+map.set("chave q nao existe", "Agora existe");
+console.log(map.has("chave q nao existe"));
+console.log(map);
+
+//Metodos keys, values e entries
+let chaves = map.keys();
+console.log(chaves);
+console.log(typeof chaves);
+console.log(chaves.next());
+console.log(chaves.next().value);
+let chave = undefined;
+while((chave = chaves.next().value)){
+    console.log("Chave -",chave);
+}
+
+let valores = map.values();
+console.log(valores);
+console.log(typeof valores);
+console.log(valores.next());
+console.log(valores.next().value);
+let valor = undefined;
+while((valor = valores.next().value)){
+    console.log("valor -",valor);
+}
+
+let chavesValores = map.entries();
+console.log(chavesValores);
+console.log(typeof chavesValores);
+console.log(chavesValores.next());
+console.log(chavesValores.next().value);
+let chaveValor = undefined;
+while((chaveValor = chavesValores.next().value)){
+    console.log("chaveValor -",chaveValor);
+}
+
+//Limpar o map
+console.log(map.clear());
+console.log(map.size);
+
+//WeakMap - Parte 1
