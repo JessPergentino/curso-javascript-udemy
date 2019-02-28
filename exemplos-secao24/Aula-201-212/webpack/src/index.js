e209aff8fb1352ffbda4e0c8b0dca82b65b2644d
JessPergentino <jessicapergentino@hotmail.com>
2019-02-28 11:35:40 -0300

    Praticas webpack css

--- exemplos-secao24/Aula-201-212/webpack/src/index.js
+++ exemplos-secao24/Aula-201-212/webpack/src/index.js
@@ -1,5 +1,5 @@
 import {} from "./instancia-Cliente";
 import {} from "./promise";
-
+import base from "./css/base.css"
 const es = "ES2015-Const";
 console.log(es);
--- exemplos-secao24/Aula-201-212/webpack/webpack.config.js
+++ exemplos-secao24/Aula-201-212/webpack/webpack.config.js
@@ -1,15 +1,33 @@
 const path = require('path');
 
 module.exports = {
-    entry: ["babel-polyfill",'./src/index.js'],
+    entry: ["babel-polyfill", './src/index.js'],
     output: {
         path: path.resolve(__dirname, "dist"),
         filename: "bundle.js"
     },
     module: {
         rules: [
-            {test: /\.js$/, exclude: /node_modules/, loader: "babel-loader"}
+            { test: /\.js$/, exclude: /node_modules/, loader: "babel-loader" },
+           /*  {
+                test: /\.css$/,
+                use: [
+                    { loader: "style-loader" },
+                    { loader: "css-loader" }
+                ]
+            } - utilizando tag style*/
+            {
+                test: /\.css$/,
+                use: [
+                    { loader: "style-loader/url" },
+                    { loader: "file-loader" }
+                ]
+            }
         ]
+    },
+    //Configuração do devServer
+    devServer: {
+        contentBase: './dist',
+        port: 9000
     }
-
 };
