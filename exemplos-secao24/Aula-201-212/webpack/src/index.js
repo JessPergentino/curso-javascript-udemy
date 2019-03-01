8d46ca776f64e8e0713b26b1157a36f9f0f9a4a8
JessPergentino <jessicapergentino@hotmail.com>
2019-02-28 21:51:18 -0300

    Praticas webpack

--- exemplos-secao24/Aula-201-212/webpack/src/index.js
+++ exemplos-secao24/Aula-201-212/webpack/src/index.js
@@ -1,5 +1,8 @@
 import {} from "./instancia-Cliente";
 import {} from "./promise";
 import base from "./css/base.css"
+import scss from './scss/base.scss'
+import 'bootstrap'
+
 const es = "ES2015-Const";
 console.log(es);
--- exemplos-secao24/Aula-201-212/webpack/webpack.config.js
+++ exemplos-secao24/Aula-201-212/webpack/webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
     entry: ["babel-polyfill", './src/index.js'],
@@ -6,22 +7,40 @@ module.exports = {
         path: path.resolve(__dirname, "dist"),
         filename: "bundle.js"
     },
+    plugins: [
+        new MiniCssExtractPlugin({
+            filename: "css.css"
+        })
+    ],
     module: {
         rules: [
             { test: /\.js$/, exclude: /node_modules/, loader: "babel-loader" },
-           /*  {
-                test: /\.css$/,
-                use: [
-                    { loader: "style-loader" },
-                    { loader: "css-loader" }
-                ]
-            } - utilizando tag style*/
             {
                 test: /\.css$/,
                 use: [
-                    { loader: "style-loader/url" },
+                    { loader: MiniCssExtractPlugin.loader }, //usado no lugar de style-loader
                     { loader: "file-loader" }
                 ]
+            },
+            //Configuração para usar o bootstrap com sass
+            {
+                test: /\.(scss)$/,
+                use: [{
+                    loader: MiniCssExtractPlugin.loader, //usado no lugar de style-loader 
+                }, {
+                    loader: 'css-loader', // translates CSS into CommonJS modules
+                }, {
+                    loader: 'postcss-loader', // Run postcss actions
+                    options: {
+                        plugins: function () { // postcss plugins, can be exported to postcss.config.js
+                            return [
+                                require('autoprefixer')
+                            ];
+                        }
+                    }
+                }, {
+                    loader: 'sass-loader' // compiles Sass to CSS
+                }]
             }
         ]
     },
